5d1df7670a8be478e302a1d9fc513bee8e382e2b
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-03-19 20:34:32 +0800

    complete the CRUD operations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,28 @@ class PostController extends Controller
 
     public function store(Request $request) {
         $request->validate([
-            'post' => 'required',
+            'post' => 'required | string ',
         ]);
 
         return Post::create($request->all());
 
+    }
+
+    public function update(Request $request, $id) {
+        // validate request data
+        $validatedData = $request->validate([
+            'post' => 'required | string ',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($validatedData);
+        return $post;
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return Post::all();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
